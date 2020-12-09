d830f875b781ec79d490b71dad63656f79f95328
TsiglerMikhail <kirtermobile@gmail.com>
2020-12-09 20:49:20 +0400

    add_card_validate

--- src/main/java/ru/example/Main.java
+++ src/main/java/ru/example/Main.java
@@ -2,18 +2,18 @@ package ru.example;
 
 import ru.example.atm.Atm;
 import ru.example.atm.Card;
+import ru.example.atm.ValidateInputException;
 import ru.example.bank.Currency;
 
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ValidateInputException {
         Card testCard = new Card("1234567894","1238" );
         Atm testAtm = new Atm("123",true, 99999);
         System.out.println(testAtm.showBalance(testCard));
         System.out.println(testAtm.giveOutCash(testCard, 100));
         System.out.println(testAtm.showBalance(testCard));
-
 
     }
 }
--- src/main/java/ru/example/atm/Card.java
+++ src/main/java/ru/example/atm/Card.java
@@ -1,13 +1,29 @@
 package ru.example.atm;
 
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+//@AllArgsConstructor
 public class Card {
 
     private final String cardNumber;
     private final String pin;
 
+    public Card(String cardNumber, String pin) throws ValidateInputException{
+        // 292Шилд
+        if(cardNumber.matches("^[0-9]{16}$")) {
+            this.cardNumber = cardNumber;
+        }
+        else {
+            throw new ValidateInputException("Не корректный номер карты");
+        }
+        if (pin.matches("^[0-9]{4}$")) {
+            this.pin = pin;
+        }
+        else {
+            throw new ValidateInputException("Не корректный пин");
+        }
+
+    }
 }
